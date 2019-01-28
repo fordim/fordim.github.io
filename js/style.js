745b4ca6f129b18e27f5f52b755be07ff60156ae
fordim <dvformos@gmail.com>
2019-01-28 10:21:10 +0300

    change project

---
+++ js/style.js
@@ -0,0 +1,32 @@
+/*
+var updateCards = function (gameBlocks){
+    var elements = document.querySelectorAll('.gameBlockLetter');
+
+    for (var i = 0, i < elements.length; i++) {
+       var element = elements[i];
+    }
+    console.log(element);
+}
+
+
+var cardList = document.querySelector('.gameBlock');
+    console.log(cardList.children);
+*/
+/*
+
+var gameTable = Array(9).fill(null);
+
+
+/*
+var myElement = document.getElementByClassName('changeText');
+myElement.textContent = 'newText';
+*/
+/*
+var myElement = document.getElementbyId('myspan');
+myElement.textContent = 'newText';
+*/
+
+/*
+document.getElementById("myspan").textContent="newtext";
+
+*/

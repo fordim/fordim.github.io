658c3721a61e6b3fdaec3f47f4688a3adf38aa35
Fordim <dvformos@gmail.com>
2019-03-11 11:09:09 +0300

    drow, onclick

--- js/style.js
+++ js/style.js
@@ -1,32 +1,88 @@
+
+
+var gameTable = ['X', null,'X','X','O','X','X',null,'X'];
+
+var draw = function(state){
+    //to do (Завернуть в цикл)
+    var gameBlocks = document.getElementsByClassName('gameBlock');
+
+    for ( var i = 0; i < gameBlocks.length; i++){
+        gameBlocks[i].innerText = state[i];
+    }
+};
+
+
+var onPageLoaded = function(){
+
+    var myElement = document.getElementById('myspan');
+    myElement.innerText = 'newText';
+
+//    updateCards();
+
+    var gameBlocks = document.getElementsByClassName('gameBlock');
+
+    for (var i = 0; i < gameBlocks.length; i++){
+        gameBlocks[i].onclick = function(e){
+            e.target.innerText = 'X';
+            console.log(e);
+            //Поменять gameTable в соответствии куда я попал (понять что за ID от 0 до 9) и туда вставить значение (X O)
+
+            //draw(gameTable);
+        };
+    }
+
+    draw(gameTable);
+
+};
+
+
 /*
-var updateCards = function (gameBlocks){
+var updateCards = function (){
     var elements = document.querySelectorAll('.gameBlockLetter');
 
-    for (var i = 0, i < elements.length; i++) {
-       var element = elements[i];
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        element.innerText = 'O';
+
     }
     console.log(element);
+};
+*/
+
+
+document.addEventListener('DOMContentLoaded', onPageLoaded);
+
+
+/*
+var a = function(number1, number2){
+    var summary = number1 * number2;
+    return summary
+};
+
+a(2,3);
+
+
+function a(number1){
+    var currentSum = number1;
+
+    function f(number2){
+        currentSum *= number2;
+        return f;
+    }
+
+    f.toString = function(){
+        return currentSum;
+    };
+
+    return f;
 }
 
-
-var cardList = document.querySelector('.gameBlock');
-    console.log(cardList.children);
-*/
-/*
-
-var gameTable = Array(9).fill(null);
+a(2)(3);
 
 
-/*
-var myElement = document.getElementByClassName('changeText');
-myElement.textContent = 'newText';
-*/
-/*
-var myElement = document.getElementbyId('myspan');
-myElement.textContent = 'newText';
-*/
-
-/*
-document.getElementById("myspan").textContent="newtext";
-
+function multi(a) {
+  return function(b) {
+  	return a * b;
+  }
+}
 */
